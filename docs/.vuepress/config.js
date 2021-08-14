94f079769a7bc0a695eb12c2f097bd1979685b39
陈柱聪 <1104201809@qq.com>
2021-08-14 12:42:54 +0800

    upload

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,14 +1,14 @@
-const head = require('./config/head.js');
-const plugins = require('./config/plugins.js');
-const themeConfig = require('./config/themeConfig.js');
+const head = require("./config/head.js");
+const plugins = require("./config/plugins.js");
+const themeConfig = require("./config/themeConfig.js");
 
 module.exports = {
-  theme: 'vdoing', // 使用npm包主题
+  theme: "vdoing", // 使用npm包主题
   // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
 
   title: "懒虫波尔",
-  description: '懒虫波尔的个人博客，主要用于总结前端，后端，学习笔记',
-  // base: '/', // 格式：'/<仓库名>/'， 默认'/'
+  description: "懒虫波尔的个人博客，主要用于总结前端，后端，学习笔记",
+  base: "/blog/", // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
   },
@@ -16,4 +16,4 @@ module.exports = {
   head,
   plugins,
   themeConfig,
-}
+};
