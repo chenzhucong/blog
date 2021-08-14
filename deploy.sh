6f8bdcfce92744e6af4ba8b879522f2748dbcb25
陈柱聪 <1104201809@qq.com>
2021-08-14 11:51:32 +0800

    upload

--- deploy.sh
+++ deploy.sh
@@ -9,35 +9,34 @@ npm run build
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
-# deploy to github pages
-echo 'b.xugaoyi.com' > CNAME
-
-if [ -z "$GITHUB_TOKEN" ]; then
+# deploy to github
+#echo 'b.miluluyo.github.io' > CNAME
+if [ -z "$VDOING" ]; then
   msg='deploy'
   githubUrl=git@github.com:chenzhucong/blog.git
 else
-  msg='来自github actions的自动部署'
-  githubUrl=https://xugaoyi:${GITHUB_TOKEN}@github.com/xugaoyi/vuepress-theme-vdoing.git
-  git config --global user.name "xugaoyi"
-  git config --global user.email "894072666@qq.com"
+  msg='来自 github actions的自动部署'
+  githubUrl=https://miluluyo:${VDOING}@github.com/miluluyo/vdoingBlog.git
+  git config --global user.name "miluluyo"
+  git config --global user.email "miluluyo@163.com"
 fi
 git init
 git add -A
 git commit -m "${msg}"
-git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
+git push -f $githubUrl master:gh-pages # 推送到github
+#git push -f git@github.com:miluluyo/vdoingBlog.git master:gh-pages
 
-# deploy to coding pages
-# echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
-# echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
 
-# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-#   codingUrl=git@e.coding.net:xgy/xgy.git
-# else
-#   codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/xgy/xgy.git
-# fi
-# git add -A
-# git commit -m "${msg}"
-# git push -f $codingUrl master # 推送到coding
+# deploy to coding
+#echo 'www.miluluu.com\miluluu.com' > CNAME  # 自定义域名
+#if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+#  codingUrl=git@e.coding.net:miluluu/miluluu.git
+#else
+#  codingUrl=https://miluluu:${CODING_TOKEN}@e.coding.net/miluluu/miluluu/miluluu.git
+#fi
+#git add -A
+#git commit -m "${msg}"
+#git push -f $codingUrl master # 推送到coding
 
-cd -
+cd - # 退回开始所在目录
 rm -rf docs/.vuepress/dist
